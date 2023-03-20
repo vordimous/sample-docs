a613326f1047dc5c67068c8a3f8348b47fd1a814
AJ Danelz <vordimous@outlook.com>
2023-03-20 14:50:08 -0400

    Update theme.ts

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -3,7 +3,7 @@ import { enNavbar, zhNavbar } from "./navbar/index.js";
 import { enSidebar, zhSidebar } from "./sidebar/index.js";
 
 export default hopeTheme({
-  hostname: "https://vuepress-theme-hope-docs-demo.netlify.app",
+  hostname: process.env.SITE_URL || "https://vordimous.gitub.io",
 
   author: {
     name: "Mr.Hope",
