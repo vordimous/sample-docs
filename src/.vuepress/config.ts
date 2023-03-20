1223939b2cf0f7a7ce9dc94fd0a2da19c7cc58f0
AJ Danelz <vordimous@outlook.com>
2023-03-20 15:01:12 -0400

    Update config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/sample-docs/",
+  base: process.env.SITE_BASE || "/",
 
   locales: {
     "/": {
