7a34d24043d1df60eb51e92a5d0d10e06aa10330
AJ <vordimous@outlook.com>
2023-03-27 16:57:27 -0400

    Implement bare bones blog structure

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,23 +2,15 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/sample-docs/",
+  base: "/",
 
   locales: {
     "/": {
       lang: "en-US",
-      title: "Docs Demo",
-      description: "A docs demo for vuepress-theme-hope",
-    },
-    "/zh/": {
-      lang: "zh-CN",
-      title: "文档演示",
-      description: "vuepress-theme-hope 的文档演示",
+      title: "vuepress-theme-hope",
+      description: "A VuePress theme with tons of features✨",
     },
   },
 
   theme,
-
-  // Enable it with pwa
-  // shouldPrefetch: false,
 });
